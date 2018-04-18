ee2ba094fbad841f3781b4bcb6d79250eedc49b9
Joe Slingsby <Joe.slingsby@3tcsoftware.com>
2018-04-18 11:32:31 +0100

    Spacing

--- src/components/DiscordWidgets.js
+++ src/components/DiscordWidgets.js
@@ -3,8 +3,8 @@ import React from 'react';
 function DiscordWidgets(props) {
     return (
         <div>
-            <iframe title="Nixzi's Discord" src="https://discordapp.com/widget?id=311205249878720513&theme=dark" width="350" height="500" allowtransparency="true" frameBorder="0"></iframe>
-            <iframe title="Nixzi's Discord" src="https://discordapp.com/widget?id=332863577121619968&theme=dark" width="350" height="500" allowtransparency="true" frameBorder="0"></iframe>
+            <iframe title="Nixzi Maguire" src="https://discordapp.com/widget?id=311205249878720513&theme=dark" width="350" height="500" allowtransparency="true" frameBorder="0"></iframe>
+            <iframe title="Mad Murlocs" src="https://discordapp.com/widget?id=332863577121619968&theme=dark" width="350" height="500" allowtransparency="true" frameBorder="0"></iframe>
         </div>
     )
 }
